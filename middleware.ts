d1262ae462fe178f1b57da3e52d63613ea71a500
Caden Shokat <cadenshokat@Cadens-MacBook-Air-2.local>
2026-01-14 19:09:42 -0500

    middleware bug fix

--- middleware.ts
+++ middleware.ts
@@ -9,8 +9,8 @@ export async function middleware(request: NextRequest) {
 export const config = {
   matcher: [
     /*
-     * Match all request paths except static files
+     * Match all request paths except static files and images
      */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.svg|.*\\.png|.*\\.jpg|.*\\.jpeg|.*\\.gif|.*\\.webp).*)',
   ],
 }
